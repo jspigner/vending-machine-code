8aae9f971d11551c367597f5a6bcba8b5af4ab99
Julie Spigner <77714712+jspigner@users.noreply.github.com>
2021-10-10 20:33:45 -0700

    Create vending-machine-code.py

---
+++ vending-machine-code.py
@@ -0,0 +1,23 @@
+# Create a class titled Soda_Machine 
+# Add variables (paid, balance) 
+# Define features (eject_soda, pay, self_soda)  
+
+class Soda_Machine:
+  paid = False
+  balance = 0
+
+  def eject_soda(self):
+    if paid == False:
+      print('Please insert money')
+    else:
+      print('Enjoy the soda!')
+
+  def pay(self, amount):
+    self.balance += amount
+    
+  def select_soda(self):
+    if balance >= 1:
+      self.paid = True
+      self.balance -= 1
+    else:
+      self.paid = False
